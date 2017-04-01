b663217b2c1818ca164d845a66170552100fac6a
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-04-01 01:41:33 -0700

    prepare flask login

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 
 from flask import Flask
 from flask_pymongo import PyMongo
+import flask_login
 
 app = Flask(__name__)
+
+# mongodb
 mongo = PyMongo(app)
+
+# login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+class User(flask_login.UserMixin):
+    pass
 
 
 @app.route("/")
 def index():
     return "Welcome to Hymnia"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    pass
 
 
 if __name__ == '__main__':
