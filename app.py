d52bb189f0e61f62d32f40e17a033ab998592add
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-04-01 02:53:15 -0700

    add login, then disable it

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 
+import os
+import flask
 from flask import Flask
 from flask_pymongo import PyMongo
 import flask_login
+from flask_login import login_required, LoginManager
+
 
 app = Flask(__name__)
+if os.getenv("SECRET_KEY", None):
+    app.secret_key = os.getenv("SECRET_KEY")
+else:
+    raise ValueError("No SECRET_KEY")
 
 # mongodb
 mongo = PyMongo(app)
 
 # login
-login_manager = flask_login.LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
 
 
 class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    # get user from db
+    user_json = mongo.db.users.find({"email": email})
+    print(user_json)
+    user = User()
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
     pass
 
 
@@ -24,7 +46,28 @@ def index():
 
 @app.route("/login", methods=["POST"])
 def login():
-    pass
+    """POST JSON to /login, make sure `email` key is present"""
+    req_json = flask.request.json
+    print(req_json)
+
+    # flask_login user object
+    user = User()
+    user.id = req_json["email"]
+    # NO password check: NO password hashing, NO salt
+    flask_login.login_user(user)
+    return "Logged in"
+
+
+@app.route("/logout")
+def logout():
+    flask_login.logout_user()
+    return "Logged out"
+
+
+@app.route("/board")
+# @login_required                 # no login needed
+def status():
+    return "Good to be logged in"
 
 
 if __name__ == '__main__':
